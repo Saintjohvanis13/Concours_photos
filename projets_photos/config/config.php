0ceb570308556ee43f29d292bfda5c8a5617c0c4
etudiant <etudiant@I4-virtu>
2026-06-04 15:12:01 +0200

    sup

--- projets_photos/config/config.php
+++
@@ -1,6 +0,0 @@
-
-<?php
-if (!defined('HOST')) define('HOST', '192.168.156.221');
-if (!defined('DB')) define('DB', 'projet1_tp2');
-if (!defined('USER')) define('USER', 'admin_projet1_tp2');
-if (!defined('PASSWORD')) define('PASSWORD', 'BbX!r4zw');
