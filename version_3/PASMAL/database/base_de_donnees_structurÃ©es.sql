56a660ad09bb7ecd2f87ff94e8d7d99df78d78c4
ckNao <naomiekarim2007@gmail.com>
2026-06-08 15:38:55 +0200

    Create 'photo' table for storing photo details
    Added a new table 'photo' to store photo metadata.

--- version_3/PASMAL/database/base_de_donnees_structurÃ©es.sql
+++ version_3/PASMAL/database/base_de_donnees_structurÃ©es.sql
@@ -35,14 +35,16 @@ CREATE TABLE configuration (
     valeur DATE NOT NULL
 );
 
+CREATE TABLE photo (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    nom_fichier VARCHAR(255) NOT NULL,
+    date_depot DATETIME NOT NULL,
+    id_etu INT NOT NULL,
+    FOREIGN KEY (id_etu) REFERENCES etudiant(id)
+);
+
 INSERT INTO configuration (parametre, valeur) VALUES
 ('depot_debut', '2026-09-07'),
 ('depot_fin', '2026-09-18'),
-('vote1_debut', '2026-09-28'),
-('vote1_fin', '2026-10-03'),
-('vote2_debut', '2026-10-04'),
-('vote2_fin', '2026-10-06'),
-('resultat1_debut', '2026-10-05'),
-('resultat1_fin', '2026-10-11'),
-('resultat2_debut', '2026-10-05'),
-('resultat2_fin', '2026-10-11');
+
+
